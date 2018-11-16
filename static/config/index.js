b1cdb010128957581d954fddb30b8d3ad033594a
qweaa <2275414653@qq.com>
2018-11-16 16:08:20 +0800

    配置文件

---
+++ static/config/index.js
@@ -0,0 +1,12 @@
+const address = '192.168.3.253'
+const port = 9093
+
+const APP_CONFIG = {
+    server: {
+        address: address,
+        port: port,
+        host: 'http://'+address+':'+port
+    },
+}
+
+export default APP_CONFIG
